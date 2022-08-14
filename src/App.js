0e0e5e3d4942832d7e7139a821cd761e58ee059d
haniframadhani <mhaniframadhani985@gmail.com>
2022-08-14 21:18:34 +0700

    hide api key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Card from './component/Card';
 import React, { useState } from 'react';
 function App() {
   const [bg, setBg] = useState('');
-  console.log(process.env.REACT_APP_NASA_API_KEY)
   return (
     <div className="App" style={{
       backgroundImage: `url(${bg})`
--- src/component/ConstructorApi.js
+++ src/component/ConstructorApi.js
@@ -1,3 +1,3 @@
-const ConstructorApi = date => `https://api.nasa.gov/planetary/apod?api_key=5kgbBpQYQXWsJd7DVGtS5aImLPgFZVcKaV5IFAFA&thumbs=true&date=${date.length != 0 ? `${date}` : ""}`
+const ConstructorApi = date => `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&thumbs=true&date=${date.length != 0 ? `${date}` : ""}`
 
 export default ConstructorApi;
